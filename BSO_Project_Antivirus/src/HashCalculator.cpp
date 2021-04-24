e9c92c8d144283726d8d12c94c59eee114b26928
Szymon Miareczka <01149372@pw.edu.pl>
2021-04-24 14:21:24 +0200

    Get hash of file(filename)

--- BSO_Project_Antivirus/src/HashCalculator.cpp
+++ BSO_Project_Antivirus/src/HashCalculator.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
-#include <fstream>
+#include<iostream>
+#include<string>
+#include<cstdio>
+#include<memory>
+#include<stdexcept>
+#include<array>
+#include<stdio.h>
+#include<cstring>
+
 using namespace std;
+string GetFileHash(string filename);
 
 
-int  
+int main(){
+	std::cout << "Hello World!\n";
+	string file = "main.cpp";
+	string cmdin =  GetFileHash(file);	
+	std::cout << "Hash value is: " ;
+	std::cout<< cmdin;
+	
+	if(cmdin !="e998cfb6782f241601ac1cbee2862f3d7229bf1aca0b7c808cf521a2b7c4b61b"){
+		cout<<"\nDziala";}
+	return 0;
+}
+
+string GetFileHash(string filename){
+	string cmd = "sha256sum " + filename;
+	string data;
+	FILE * stream;
+	const int max_buffer= 256;
+	char buffer[max_buffer];
+	cmd.append(" 2>&1");
+	stream = popen(cmd.c_str(), "r");
+	if(stream){
+		while(!feof(stream))
+			if(fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+		pclose(stream);
+	}
+	return data.substr(0,64);
+	}
+
--- BSO_Project_Antivirus/src/main.cpp
+++ BSO_Project_Antivirus/src/main.cpp
@@ -2,14 +2,15 @@
 #include<string>
 #include<stdio.h>
 #include<stdlib.h>
+#include<crypto>
 
 using namespace std;
 
 int main(){
-	cout << "Hello World!";
+	cout << "Hello World!\n";
 	
-	int cmdin = system("sha256sum ../virus_hash_database/file1.txt");
-	cout << "Hash value is" ;
+	string cmdin = to_string(system("sha256sum --text ../virus_hash_database/file1.txt")).substr(0,64);
+	cout << "Hash value is: " ;
 	cout<< cmdin;
 	
 	
