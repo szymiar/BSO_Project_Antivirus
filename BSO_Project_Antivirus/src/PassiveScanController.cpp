5d5a3cce0f6c3bf04146d650a4e3f45dfd8c5d1e
Szymon Miareczka <56802664+szymiar@users.noreply.github.com>
2021-05-22 14:24:52 +0200

    Create PassiveScanController.cpp

---
+++ BSO_Project_Antivirus/src/PassiveScanController.cpp
@@ -0,0 +1,34 @@
+#include "../include/AntivirusController.h"
+#include "../include/FileHandler.h"
+namespace fs = std::filesystem;
+
+//const string FoldersList;
+
+void TurnOnPassiveScan(){
+	//Do something
+	}
+	
+
+
+void TurnOffPassiveScan(){
+	//Do something
+	}
+
+
+void AddFolderToPassiveScan(std::string foldername){
+    //AppendToFile(foldername, FoldersList);
+  
+  }
+
+
+void DisplayPassiveScanFoldersList(){
+    //Display folder content
+  
+  }
+
+
+void RemoveFolderFromPassiveScan(std::string foldername){
+  //RemoveFromFile(foldername, FoldersList);
+  
+}
+
