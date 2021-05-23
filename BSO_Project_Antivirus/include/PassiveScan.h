5d496dd5154f00f547fbb488f5c042b249728f0b
Szymon Miareczka <56802664+szymiar@users.noreply.github.com>
2021-05-23 14:10:46 +0200

    Create PassiveScan.h

---
+++ BSO_Project_Antivirus/include/PassiveScan.h
@@ -0,0 +1,22 @@
+#ifndef PASSIVESCAN_H
+#define PASSIVESCAN_H
+
+#include "FileHandler.h"
+
+class PassiveScan
+{
+private:
+static PassiveScan *instance;
+
+std::vector<std::string> PassiveScanList;
+
+PassiveScan();
+public: 
+ static PassiveScan *GetInstance();
+std::vector<std::string> GetPassiveScanList();
+
+void AddFolder(std::string foldername);
+
+};
+
+#endif
