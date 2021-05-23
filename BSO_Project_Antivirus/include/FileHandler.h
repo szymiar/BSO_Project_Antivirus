8969f57702df7385d57f3efe5b844e87fbb888ab
Szymon Miareczka <56802664+szymiar@users.noreply.github.com>
2021-05-23 10:04:27 +0200

    Update FileHandler.h

--- BSO_Project_Antivirus/include/FileHandler.h
+++ BSO_Project_Antivirus/include/FileHandler.h
@@ -13,6 +13,13 @@ std::vector<std::string> ReadFile(std::string filename);
 
 void AppendToFile(std::vector<std::string> hashes,std::string filename);
 
+void AppendToFile(std::string foldername, std::string filename);
+
+
+void RemoveFromFile(std::string foldername, std::string filename);
+
+void ClearFile(std::string filename);
+
 bool CheckFileExistence(std::string filename);
 
 bool CheckFolderExistence(std::string filename);
