341cd2528b02b00e9dfc53a8cf2f5dcb863f6dcc
Szymon Miareczka <56802664+szymiar@users.noreply.github.com>
2021-05-23 14:41:29 +0200

    Update QuarantineServiceHandler.cpp

--- BSO_Project_Antivirus/src/QuarantineServiceHandler.cpp
+++ BSO_Project_Antivirus/src/QuarantineServiceHandler.cpp
@@ -4,8 +4,10 @@
 
 //Move dangerous file to safe folder that can not be easilly accessed
 void MoveFileToSafety(std::string filename){
-	std::string cmd = "mv " + filename + " " + QuarantineFolder;
-	system(cmd.c_str());
+	ifstream ifs(filename, ios::in | ios::binary);
+	ofstream ofs(QuarantineFolder, ios::out | ios::binary);
+	ofs << ifs.rdbuf();
+	remove(oldname);
 }
 
 //Change file permissions so that only privileged entity can use it
