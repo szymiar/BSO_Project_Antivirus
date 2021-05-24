55a3733a9503e347568c5488922545cf0136be8f
Szymon Miareczka <01149372@pw.edu.pl>
2021-05-24 21:51:18 +0200

    Fixing mistakes

--- BSO_Project_Antivirus/src/QuarantineServiceHandler.cpp
+++ BSO_Project_Antivirus/src/QuarantineServiceHandler.cpp
@@ -5,8 +5,10 @@
 //Move dangerous file to safe folder that can not be easilly accessed
 void MoveFileToSafety(std::string filename){
 	std::ifstream ifs(filename, std::ios::in | std::ios::binary);
-	std::string newname = QuarantineFolder + "/" + filename;
-	std::ofstream ofs(QuarantineFolder, std::ios::out | std::ios::binary);
+	std::size_t index = filename.find_last_of("/");
+	std::string substr = filename.substr(index+1,filename.size()+1);	
+	std::string newname = QuarantineFolder + "/" + substr;
+	std::ofstream ofs(newname, std::ios::out | std::ios::binary);
 	ofs << ifs.rdbuf();
 	std::remove(filename.c_str());
 }
@@ -14,5 +16,4 @@ void MoveFileToSafety(std::string filename){
 //Change file permissions so that only privileged entity can use it
 void ChangeFilePermissions(std::string filename){
 	chmod(filename.c_str(), S_IRUSR);
-
 }
