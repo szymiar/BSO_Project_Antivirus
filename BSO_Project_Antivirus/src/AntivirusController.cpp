1da71c13e222488478303a013d43b6e2c29afeba
Szymon Miareczka <01149372@pw.edu.pl>
2021-05-25 14:32:09 +0200

    Everything except /proc works

--- BSO_Project_Antivirus/src/AntivirusController.cpp
+++ BSO_Project_Antivirus/src/AntivirusController.cpp
@@ -30,7 +30,7 @@ void ScanPackage(std::string path){
 	for(auto&  dirEntry :  fs::recursive_directory_iterator(path,fs::directory_options::skip_permission_denied)){
 
 		filename = dirEntry.path();
-		if(filename.rfind("/sys/kernel/security/apparmor/revision",0)==0   || filename.rfind("/dev/core",0)==0 || filename.rfind("/sys/kernel/debug",0)==0|| filename.rfind("/proc/kmsg",0)==0 || filename.rfind("/proc/kcore",0)==0 ){
+		if(filename.rfind("/sys/kernel/security/apparmor/revision",0)==0   || filename.rfind("/dev/core",0)==0 || filename.rfind("/sys/kernel/debug",0)==0|| filename.rfind("/proc/kmsg",0)==0 || filename.rfind("/proc/kcore",0)==0 || filename.rfind("/proc/",0)==0 ){
 			continue;
 		}
 		std::cout<<"\nCheck 2\n";
