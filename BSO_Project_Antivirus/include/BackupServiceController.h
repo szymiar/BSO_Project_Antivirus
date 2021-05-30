093b61872990072d2fad2921c0e87ac536273a6c
Szymon Miareczka <01149372@pw.edu.pl>
2021-05-30 23:22:32 +0200

    Getting statistics from any terminal implemented and tested

--- BSO_Project_Antivirus/include/BackupServiceController.h
+++ BSO_Project_Antivirus/include/BackupServiceController.h
@@ -8,7 +8,7 @@
 #include <thread>
 
 const std::string BackupFolderListPath = "/usr/local/share/BSO_Project_Antivirus/data/BackupService/BackupFolderList.txt";
-const std::string BackupFolderPath = "/usr/local/share/BSO_Project_Antivirus/data/BackupService/BackupFolder";
+const std::string BackupFolderPath = "/usr/local/share/BSO_Project_Antivirus/data/BackupService/BackupFolder/";
 void AddFileToBackup(std::string filename);
 
 void RemoveFileFromBackup(std::string filename);
--- BSO_Project_Antivirus/include/QuarantineServiceHandler.h
+++ BSO_Project_Antivirus/include/QuarantineServiceHandler.h
@@ -8,7 +8,7 @@
 #include <fstream>
 #include <iostream>
 
-const std::string QuarantineFolder="/usr/local/share/BSO_Project_Antivirus/quarantine";
+const std::string QuarantineFolder="/usr/local/share/BSO_Project_Antivirus/quarantine/";
 void MoveFileToSafety(std::string filename);
 void ChangeFilePermissions(std::string filename);
 
