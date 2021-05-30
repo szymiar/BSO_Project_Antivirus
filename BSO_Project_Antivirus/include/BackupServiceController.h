1ccf4a9a7fddc963d9a02e32db523a741932fdb9
Szymon Miareczka <56802664+szymiar@users.noreply.github.com>
2021-05-30 22:42:34 +0200

    Update BackupServiceController.h

--- BSO_Project_Antivirus/include/BackupServiceController.h
+++ BSO_Project_Antivirus/include/BackupServiceController.h
@@ -7,8 +7,8 @@
 #include <unistd.h>
 #include <thread>
 
-const std::string BackupFolderListPath = "data/BackupService/BackupFolderList.txt";
-const std::string BackupFolderPath = "data/BackupService/BackupFolder";
+const std::string BackupFolderListPath = "/usr/local/share/BSO_Project_Antivirus/data/BackupService/BackupFolderList.txt";
+const std::string BackupFolderPath = "/usr/local/share/BSO_Project_Antivirus/data/BackupService/BackupFolder";
 void AddFileToBackup(std::string filename);
 
 void RemoveFileFromBackup(std::string filename);
